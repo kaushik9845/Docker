dffc3e4ff388274c7c28d6dd392b18c61e5c1f0f
kaushik9845 <kaushik9845@gmail.com>
2022-03-08 19:48:18 +0530

    Update Customer.java

--- SpringBoot-Mongo/src/main/java/com/example/accessingdatajpa/Customer.java
+++ SpringBoot-Mongo/src/main/java/com/example/accessingdatajpa/Customer.java
@@ -1,16 +1,12 @@
 package com.example.accessingdatajpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 
 @Entity
 public class Customer {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	private String id;
 	private String firstName;
 	private String lastName;
 
