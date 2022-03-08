5d939e4c10e63c70378472b75e8ff7a8443f9eb4
kaushik9845 <kaushik9845@gmail.com>
2022-03-08 19:45:49 +0530

    Update CustomerRepository.java

--- SpringBoot-Mongo/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ SpringBoot-Mongo/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -2,9 +2,9 @@ package com.example.accessingdatajpa;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.MongoRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends MongoRepository<Customer, Long> {
 
 	List<Customer> findByLastName(String lastName);
 
